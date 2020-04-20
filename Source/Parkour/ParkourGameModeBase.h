f0e06d57005c50eab980ae5b5d05263730ed5979
Богдан Левченко <bodya.levchenko20@gmail.com>
2020-04-20 16:21:36 +0300

    first commit

---
+++ Source/Parkour/ParkourGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParkourGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOUR_API AParkourGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
